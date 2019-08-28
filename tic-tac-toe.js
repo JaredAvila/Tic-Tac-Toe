ee7c5ab239a1f04c4a0456eb771376fdedbbe9ee
Jared Avila <jared.c.avila@gmail.com>
2019-08-28 09:47:17 -0700

    setting up game logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,8 +4,38 @@ const startOver = document.querySelector(".startOver");
 
 playBtn.addEventListener("click", () => {
   play.classList.add("hidden");
+  startGame();
 });
 
 startOver.addEventListener("click", () => {
   play.classList.remove("hidden");
 });
+
+class Player {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const startGame = () => {
+  // Get players names
+  let playerOneName = prompt(
+    "Enter first player's name, please.",
+    "Player One"
+  );
+  let playerTwoName = prompt(
+    "Enter second player's name, please.",
+    "Player Two"
+  );
+  // If no name provided then give default values
+  if (!playerOneName) {
+    playerOneName = "Player One";
+  }
+  if (!playerTwoName) {
+    playerTwoName = "Player Two";
+  }
+  // Instantiate players
+  const playerOne = new Player(playerOneName);
+  const playerTwo = new Player(playerTwoName);
+  console.log(playerOne, playerTwo);
+};
