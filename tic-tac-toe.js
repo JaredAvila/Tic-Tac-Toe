b284494de6f1657f8f212e472954c8c960b86788
Jared Avila <jared.c.avila@gmail.com>
2019-08-28 10:10:18 -0700

    createPlayers func done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,23 +1,43 @@
+// DOM elements
+
 const playBtn = document.querySelector(".playBtn");
 const play = document.querySelector(".play");
 const startOver = document.querySelector(".startOver");
+const playersTurnDisp = document.querySelector(".playerName");
 
+// Players
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.turn = false;
+    this.winner = false;
+    this.movesGrid = [
+      [false, false, false],
+      [false, false, false],
+      [false, false, false]
+    ];
+  }
+}
+
+// When user pushes 'Play' run startgame function
 playBtn.addEventListener("click", () => {
+  // Hide play button screen
   play.classList.add("hidden");
   startGame();
 });
 
+// Takes user back to 'main menu'
 startOver.addEventListener("click", () => {
+  // Show play button screen
   play.classList.remove("hidden");
+  playersTurnDisp.classList.add("transName");
 });
 
-class Player {
-  constructor(name) {
-    this.name = name;
-  }
-}
-
 const startGame = () => {
+  createPlayers();
+};
+
+const createPlayers = () => {
   // Get players names
   let playerOneName = prompt(
     "Enter first player's name, please.",
@@ -27,6 +47,9 @@ const startGame = () => {
     "Enter second player's name, please.",
     "Player Two"
   );
+  // remove white space/prevent users from entering only spaces as a name
+  playerOneName = playerOneName.trim();
+  playerTwoName = playerTwoName.trim();
   // If no name provided then give default values
   if (!playerOneName) {
     playerOneName = "Player One";
@@ -37,5 +60,12 @@ const startGame = () => {
   // Instantiate players
   const playerOne = new Player(playerOneName);
   const playerTwo = new Player(playerTwoName);
+  playerOne.turn = true;
+  playerTurn(playerOne);
   console.log(playerOne, playerTwo);
+};
+
+const playerTurn = player => {
+  playersTurnDisp.innerHTML = `${player.name}'s Turn`;
+  playersTurnDisp.classList.remove("transName");
 };
